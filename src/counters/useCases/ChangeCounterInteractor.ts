0ade8919a51408530d243924a1d83eed9b0b9e00
Daniel Oliveira <dvalbrand@gmail.com>
2017-07-29 14:53:46 -0300

    Adding ChangeCounterInteractor

---
+++ src/counters/useCases/ChangeCounterInteractor.ts
@@ -0,0 +1,18 @@
+import CounterGateway from '../gateways/CounterGateway';
+
+export interface ChangeCounterInteractor {
+  increment(): void;
+  decrement(): void;
+}
+
+export function createChangeCounterInteractor(counterGateway: CounterGateway): ChangeCounterInteractor {
+  return {
+    increment() {
+      counterGateway.increment();
+    },
+
+    decrement() {
+      counterGateway.decrement();
+    }
+  };
+}
